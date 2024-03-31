cb824f3bfa5166ade5adff95cb9395ead825fa73
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-03-31 18:09:45 +0530

    all API added

--- DAY-18/index.js
+++ DAY-18/index.js
@@ -16,11 +16,18 @@ app.post("/create",async (req,res)=>{
 app.get("/read", async (req,res)=>{
     let data = await product.find();
     res.send(data);
-})
+});
 
 app.put("/update/:_id",async (req,res)=>{
     let data= await product.updateOne(req.params,{$set: req.body});
     res.send(data);
+});
+
+
+app.delete("/delete/:_id", async (req,res)=>{
+    let data = await product.deleteOne(req.params);
+    res.send(data);
 })
+
 
 app.listen(5000);
