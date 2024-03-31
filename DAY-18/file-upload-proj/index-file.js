e4795eaa74b074d49c0e2c6af60b3a3497757c5c
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-03-31 18:38:46 +0530

    file upload code

---
+++ DAY-18/file-upload-proj/index-file.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const multer = require('multer')
+const app = express();
+
+const upload= multer({
+    storage: multer.diskStorage({
+        destination: function(req,res,cb){
+            cb(null, 'uploads');
+        },
+        filename: function(req, file, cb){
+            cb(null, file.fieldname+"-"+Date.now()+"-GFG"+".jpg")
+        }
+    })
+}).single('file_name')
+
+app.post("/upload",upload,(req,res)=>{
+    res.send("File Uploaded Successfully");
+});
+
+app.listen(5000);
