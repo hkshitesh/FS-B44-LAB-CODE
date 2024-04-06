0dd6179d4ac58f501e02940f7620d35fcfca2882
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-04-06 18:11:24 +0530

    nav added

--- PROJECT WORK/PROJECT WORK-FRONTEND/frontend/src/Components/Nav.js
+++ PROJECT WORK/PROJECT WORK-FRONTEND/frontend/src/Components/Nav.js
@@ -11,13 +11,26 @@ const Nav = () => {
     return(
         <div>
             <img className='logo' src="https://media.geeksforgeeks.org/wp-content/uploads/20210511160813/g4g.jpg"/>
+            {
+
+                auth ?
+
+            
             <ul className="nav-ul">
-                <li>Products</li>
-                <li>Add products</li>
-                <li>Update Products</li>
-                <li>Profile</li>
-                <li>Logout</li>                
+                <li><Link to ='/'>Products</Link></li>
+                <li><Link to ='/add'>Add products</Link></li>
+                <li><Link to ='/update'>Update Products</Link></li>
+                <li><Link to ='/profile'>Profile</Link></li>
+                <li><Link to ='/logout'>Logout</Link></li>                           
             </ul>
+            :
+            <ul className="nav-ul  nav-right">
+                <li><Link to ='/signup'>Signup</Link></li>
+                <li><Link to ='/login'>Login</Link></li>     
+            </ul>
+            
+            
+}
         </div>
         
     )}
