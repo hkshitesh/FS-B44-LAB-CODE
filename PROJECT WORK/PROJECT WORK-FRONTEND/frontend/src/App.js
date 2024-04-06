8a49fc4518261a846f4dc7c107c5d7d3170f48f4
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-04-06 18:01:05 +0530

    logo added

--- PROJECT WORK/PROJECT WORK-FRONTEND/frontend/src/App.js
+++ PROJECT WORK/PROJECT WORK-FRONTEND/frontend/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Nav from './Components/Nav';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Nav />
+      </BrowserRouter>
     </div>
   );
 }
--- PROJECT WORK/PROJECT WORK-FRONTEND/frontend/src/Components/Nav.js
+++ PROJECT WORK/PROJECT WORK-FRONTEND/frontend/src/Components/Nav.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Link, useNavigate} from 'react-router-dom'
 
 const Nav = () => {
+    const navigate = useNavigate();
+    const auth = localStorage.getItem('user');
+    const logout = ()=>{
+        localStorage.clear();
+        navigate('/signup')
+    }
     return(
         <div>
+            <img className='logo' src="https://media.geeksforgeeks.org/wp-content/uploads/20210511160813/g4g.jpg"/>
             <ul className="nav-ul">
                 <li>Products</li>
                 <li>Add products</li>
