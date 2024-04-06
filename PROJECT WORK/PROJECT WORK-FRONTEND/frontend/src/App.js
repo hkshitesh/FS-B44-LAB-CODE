a073c9ce851554747df2c6692c7108713cec728e
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-04-06 17:47:50 +0530

    Project-work

---
+++ PROJECT WORK/PROJECT WORK-FRONTEND/frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Components/Nav';
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ PROJECT WORK/PROJECT WORK-FRONTEND/frontend/src/Components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Nav = () => {
+    return(
+        <div>
+            <ul className="nav-ul">
+                <li>Products</li>
+                <li>Add products</li>
+                <li>Update Products</li>
+                <li>Profile</li>
+                <li>Logout</li>                
+            </ul>
+        </div>
+        
+    )}
+
+    export default Nav;
